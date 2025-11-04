5a89438f65e4a599ae4d1c92682f0aa2c30f81bb
kouseph <kouseph@g.ucla.edu>
2025-11-04 04:04:02 -0800

    middleware

--- afterquery_test-main/backend/app/main.py
+++ afterquery_test-main/backend/app/main.py
@@ -5,15 +5,14 @@ app = FastAPI(title="Backend API")
 
 origins = [
     "https://onboard-dun.vercel.app",
-    "https://afterquery-test.vercel.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
+# "https://afterquery-test.vercel.app",
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_origin_regex=r"https://.*\\.vercel\\.app$",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
